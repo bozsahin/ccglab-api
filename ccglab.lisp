b43799f6df1a953fd70596f6517e7274b1aaff92
Cem Bozsahin <cem.bozsahin@gmail.com>
2019-05-12 23:10:27 +0300

    added uiop package

--- ccglab.lisp
+++ ccglab.lisp
@@ -1,10 +1,11 @@
 (defpackage :ccglab
-  (:use :cl))
+  (:use :cl :uiop))
+
 
 (in-package :ccglab)
 
 (load #p"/Users/bozsahin/myrepos/ccglab/bin/suppress-style-warnings.lisp")
-(load #p"/Users/bozsahin/mysrc/lisp//lalrparser.lisp")
+(load #p"/Users/bozsahin/mysrc/lisp/lalrparser.lisp")
 (load #p"/Users/bozsahin/myrepos/ccglab/bin/ccg.lisp")
 (setf *lispsys* (read-from-string (lisp-implementation-type)))
 (reset-globals)
