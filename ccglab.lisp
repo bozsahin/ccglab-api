e3a6729380e78f5c7dc67d9d19ab0a823f9920be
Cem Bozsahin <cem.bozsahin@gmail.com>
2019-05-12 22:51:26 +0300

    just load ccglab.lisp

---
+++ ccglab.lisp
@@ -0,0 +1,13 @@
+(defpackage :ccglab
+  (:use :cl))
+
+(in-package :ccglab)
+
+(load #p"/Users/bozsahin/myrepos/ccglab/bin/suppress-style-warnings.lisp")
+(load #p"/Users/bozsahin/mysrc/lisp//lalrparser.lisp")
+(load #p"/Users/bozsahin/myrepos/ccglab/bin/ccg.lisp")
+(setf *lispsys* (read-from-string (lisp-implementation-type)))
+(reset-globals)
+(welcome *lispsys*)
+
+(in-package :cl) ; get out
