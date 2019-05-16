ea8e1a3c602d54bd8bcd28d44e97acff6323abcd
bozsahin <cem.bozsahin@gmail.com>
2019-05-16 12:49:53 +0300

    factoring api to sbcl, allegro

--- ccglab-allegro.lisp
+++ ccglab-allegro.lisp
@@ -10,7 +10,9 @@
 (load #p"/Users/bozsahin/myrepos/ccglab/bin/suppress-style-warnings.lisp")
 (load #p"/Users/bozsahin/mysrc/lisp/lalrparser.lisp")
 (load #p"/Users/bozsahin/myrepos/ccglab/bin/ccg.lisp")
-(setf *lispsys* (read-from-string (lisp-implementation-type)))
+(setf *lispsys* (read-from-string (lisp-implementation-type))) ;; Franz Inc. has funny ideas about unique names
+(pprint *lispsys*) ; showing it before reset to see what I mean
+(setf *lispsys* 'allegro) ; life is simpler this way
 (reset-globals)
 (welcome *lispsys*)
 (format t "CCGlab loaded as a package.~%Do (in-package :ccglab) to use it like in REPL")
