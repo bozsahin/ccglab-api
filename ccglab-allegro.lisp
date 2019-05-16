d9524e377bd713eab653e9dfc9068458ca2f8914
bozsahin <cem.bozsahin@gmail.com>
2019-05-16 13:12:05 +0300

    allegro run-shell-command package is excl

--- ccglab-allegro.lisp
+++ ccglab-allegro.lisp
@@ -2,7 +2,7 @@
 ;; -- cem bozsahin, 2019
 (defpackage :ccglab
   (:use :cl 
-	:ff)) ; this one is for Allegro CL
+	:excl)) ; this one is for Allegro CL's run-shell-command in this package
 
 
 (in-package :ccglab)
@@ -10,10 +10,10 @@
 (load #p"/Users/bozsahin/myrepos/ccglab/bin/suppress-style-warnings.lisp")
 (load #p"/Users/bozsahin/mysrc/lisp/lalrparser.lisp")
 (load #p"/Users/bozsahin/myrepos/ccglab/bin/ccg.lisp")
-(format t "~2%This is what Franz Inc. thinks we should remember their Lisp as:~%  ~A.~%I will remember it as ALLEGRO.~2%" 
-	(lisp-implementation-type) ;; Franz Inc. has funny ideas about unique names
-	) ; showing it before reset to see what I mean
 (setf *lispsys* 'alisp) ; life is simpler this way
+(format t "~2%This is what Franz Inc. thinks we should remember their Lisp as:~%  ~A.~%I will remember it as ~A.~2%" 
+	(lisp-implementation-type) *lispsys* ;; Franz Inc. has funny ideas about unique names
+	) ; showing it before reset to see what I mean
 (reset-globals)
 (welcome *lispsys*)
 (format t "CCGlab loaded as a package.~%Do (in-package :ccglab) to use it like in REPL")
