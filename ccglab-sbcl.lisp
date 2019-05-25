48e85bcd124add9e9bb4018798b1b129501c69e4
bozsahin <cem.bozsahin@gmail.com>
2019-05-25 08:21:23 +0300

    style fix

--- ccglab-sbcl.lisp
+++ ccglab-sbcl.lisp
@@ -13,5 +13,5 @@
 (setf *lispsys* (read-from-string (lisp-implementation-type)))
 (reset-globals)
 (welcome *lispsys*)
-(format t "CCGlab loaded as a package.~%Do (in-package :ccglab) to use it like in REPL")
+(format t "CCGlab loaded as a package.~%Do (in-package :ccglab) to use it like in REPL~%")
 (in-package :cl) ; get out
